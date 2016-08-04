fd40bc30ae98ecf274a4934839462b7a97bb1e2c
Ramiro Trejo <trejoramiro1@gmail.com>
2016-08-03 23:55:00 -0500

    Added groups controller.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/groups' => "groups#index"
+  get '/groups/new' => "groups#new"
+  post '/groups' => "groups#create"
+  get '/groups/:id' => "groups#show"
+  get '/groups/:id/edit' => "groups#edit"
+  patch '/groups/:id' => "groups#update"
+  delete '/groups/:id' => "groups#delete"
 end
