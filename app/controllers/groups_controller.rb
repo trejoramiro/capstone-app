0022a4f88eca648600ce41bcb12473ce2d94e10b
Ramiro Trejo <trejoramiro1@gmail.com>
2016-08-05 13:58:38 -0500

    Added CRUD functionality

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,2 +1,47 @@
 class GroupsController < ApplicationController
+
+
+  def index
+    @groups = Group.all
+    render 'index.html.erb'
+  end
+
+  def show
+    @group = Group.find_by(id: params[:id])
+    render 'show.html.erb'
+  end
+
+  def new
+    render 'new.html.erb'
+  end
+
+  def create
+     @group = Group.new(
+      name: params[:name],
+      location: params[:location])
+     @group.save
+    redirect_to "/groups/#{@group.id}"
+  end
+
+  def edit
+    @group = Group.find_by(id: params[:id])
+    render 'edit.html.erb'
+  end
+
+  def update
+    @group = Group.find_by(id: params[:id])
+    @group.assign_attributes(
+    { name: params[:name],
+      location: params[:location]
+      })
+    @group.save
+    redirect_to "/groups/#{@group.id}"
+  end
+
+  def delete
+    group = Group.find_by(id: params[:id])
+    group.destroy
+    redirect_to '/groups'
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,6 @@ class User < ApplicationRecord
 
     has_many :votes
     has_many :vists
+
+
 end
