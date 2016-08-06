cf4bf6da3022f3cf9d4459072b8f28a00d48898b
Ramiro Trejo <trejoramiro1@gmail.com>
2016-08-06 16:35:40 -0500

    Destroyed and dropped Restaurant Model.

--- app/models/restaurant.rb
+++
@@ -1,6 +0,0 @@
-class Restaurant < ApplicationRecord
-  belongs_to :group, optional: true
-  has_many :votes
-  has_many :visits
-  has_many :events
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160806034507) do
+ActiveRecord::Schema.define(version: 20160806213157) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,15 +40,6 @@ ActiveRecord::Schema.define(version: 20160806034507) do
     t.string   "vote_status"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
-  end
-
-  create_table "restaurants", force: :cascade do |t|
-    t.string   "name"
-    t.string   "location"
-    t.boolean  "chosen"
-    t.integer  "group_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
