ba9dbd1b91ccf20808d0b3b1b716cffa741ce61b
Ramiro Trejo <trejoramiro1@gmail.com>
2016-08-21 15:31:39 -0500

    Chatroom controller route.

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -1,2 +1,11 @@
 class ChatroomsController < ApplicationController
+  def create
+    @chatroom = Chatroom.new()
+    @chatroom.group_id = params[:group_id]
+  end
+
+  def show
+    @chatroom = Chatroom.find_by(group_id: params[:group_id])
+    @message = Message.new()
+  end
 end
