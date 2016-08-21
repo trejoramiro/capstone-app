6d41822c14a760ca58de82970a1b66c8c82d8624
Ramiro Trejo <trejoramiro1@gmail.com>
2016-08-21 15:40:29 -0500

    Added create route.

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,11 @@
 class MessagesController < ApplicationController
+  def create
+    message = Message.new(message_params)
+    message.user_id = current_user
+    if message.save
+      # do some stuff
+    else
+      # redirect to chatrooms path
+    end
+  end
 end
