6e65932e451fa8482ee635c744fe3c4c6f3e408e
Ramiro Trejo <trejoramiro1@gmail.com>
2016-08-28 16:17:29 -0500

    Cleaned files and added .eslintrc.js

--- app/assets/javascripts/appCtrl.js
+++ app/assets/javascripts/appCtrl.js
@@ -1,26 +1,20 @@
 /* global angular */
 
 (function() {
-  angular.module('app').controller('messagesCtrl', function($scope, $http) {
-
-    $scope.setUp = function () {
-      console.log("hello");
-      $http.get('/api/v1/messages.json').then(function (response) {
-
-        $scope.messages = response.data;
-      });
-
-    };
-
-    $scope.createNewMessage = function(inputMessage) {
-      var params = {
-        body: inputMessage
-      };
-      $http.post('/api/v1/messages', params).then(function(response) {
-    
-        $scope.newMessageBody = '';
-      });
-    };
-    window.$scope = $scope;
-  });
+    angular.module('app').controller('messagesCtrl', function($scope, $http) {
+        $scope.setUp = function () {
+            $http.get('/api/v1/messages.json').then(function (response) {
+                $scope.messages = response.data;
+            });
+        };
+        $scope.createNewMessage = function(inputMessage) {
+            var params = {
+                body: inputMessage
+            };
+            $http.post('/api/v1/messages', params).then(function(response) {
+                $scope.newMessageBody = '';
+            });
+        };
+        window.$scope = $scope;
+    });
 })();
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,28 +1,29 @@
+# ApplicatonController class
 class ApplicationController < ActionController::Base
-    protect_from_forgery
-    helper_method :current_user
+  protect_from_forgery
+  helper_method :current_user
 
-    after_filter :set_csrf_cookie_for_ng
+  after_filter :set_csrf_cookie_for_ng
 
-    def set_csrf_cookie_for_ng
-      cookies['XSRF-TOKEN'] = form_authenticity_token if protect_against_forgery?
-    end
+  def set_csrf_cookie_for_ng
+    cookies['XSRF-TOKEN'] = form_authenticity_token if protect_against_forgery?
+  end
 
-    def current_user
-        @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
-    end
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+  end
 
-    def authenticate_user!
-        redirect_to '/login' unless current_user
-    end
+  def authenticate_user!
+    redirect_to '/login' unless current_user
+  end
 
-    def authenticate_admin!
-        redirect_to '/' unless current_user && current_user.admin
-    end
+  def authenticate_admin!
+    redirect_to '/' unless current_user && current_user.admin
+  end
 
-    protected
+  protected
 
-    def verified_request?
-      super || valid_authenticity_token?(session, request.headers['X-XSRF-TOKEN'])
-    end
+  def verified_request?
+    super || valid_authenticity_token?(session, request.headers['X-XSRF-TOKEN'])
+  end
 end
