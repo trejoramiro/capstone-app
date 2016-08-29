5e0c46e9fde779f2e540b2cdf06d271a5cfee7d6
Ramiro Trejo <trejoramiro1@gmail.com>
2016-08-29 12:19:11 -0500

    Create image column in users.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160821200715) do
+ActiveRecord::Schema.define(version: 20160829170932) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -65,6 +65,7 @@ ActiveRecord::Schema.define(version: 20160821200715) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.string   "password_digest"
+    t.string   "profile_img"
   end
 
   create_table "venues", force: :cascade do |t|
