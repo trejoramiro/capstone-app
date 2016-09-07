91edf9342a9541506e345fd66b5ddc6b8027233c
Ramiro Trejo <trejoramiro1@gmail.com>
2016-09-07 09:59:41 -0500

    Respond to search and vote.

--- app/controllers/twilios_controller.rb
+++ app/controllers/twilios_controller.rb
@@ -24,10 +24,21 @@ def reply
 
     if message[0].upcase == "SEARCH"
 
+
+    data = Unirest.get("https://api.foursquare.com/v2/venues/explore?ll=41.895273,-87.67545799999999&query=sushi&client_id=#{ENV['CLIENT_ID']}&client_secret=#{ENV['CLIENT_SECRET']}&v=20160809").body
+    venues_data = data['response']['groups'][0]['items']
+
+    index = 0
+    message_body = ""
+    3.times do
+        message_body = message_body + venues_data[index]['venue']['name'] + "\n" + venues_data[index]['venue']['location']['address'] + "\n" + venues_data[index]['venue']['rating'].to_s +  "\n\n"
+        index = index + 1
+    end
+
     @client.messages.create(
-      from: ENV['TWILIO_NUM'],
-      to: from_number,
-      body: "You searched for " + message[1]
+    from: ENV['TWILIO_NUM'],
+    to: from_number,
+    body: message_body
     )
 
     elsif message[0].upcase == "VOTE"
@@ -36,6 +47,7 @@ def reply
         to: from_number,
         body: "You voted for " + message[1]
       )
+
     else
     @client.messages.create(
       from: ENV['TWILIO_NUM'],
